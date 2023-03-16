06f40591deadf47d391e427076c9425d1384a3a6
whit <whitspa64@gmail.com>
2023-03-16 14:21:31 -0400

    created schema directory and added files to define query and mutation functionality and export typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+//add your models as const model name = require('/."");
+
+const typeDefs = gql`
+    type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [] //array of book type
+  }
+
+  type book {
+  bookId: //value returned from Google's Book API
+  authors: [] array of strings as there will be more than one authors
+  description: String
+  title: String
+  image: 
+  link:
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    me: [User]
+  }
+
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    adduser(username: String!, email: String!, password:String!): Auth
+    saveBook(authors: String!, description: String!, title: String!, bookId: ,image: ,link: ): User
+    removeBook(bookId: ): User
+  }
+`;
+
+module.exports = typeDefs;
+//look into inpu type to handle all the savBook Mutation parameters
