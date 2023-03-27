b7e333063b167a256d9461db7115d29438d78e87
whit <whitspa64@gmail.com>
2023-03-27 11:11:20 -0400

    more work in the handleSaveBook function. Added syntax for the Try Catch block

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -80,6 +80,13 @@ const SearchBooks = () => {
     }
 
     try {
+      await saveBook({
+        variables: {book: bookToSave},
+        update: cache => {
+          const {me} = cache.readQuery({ query: GET_ME});
+        cache.writeQuery({ query: GET_ME , data: {me: {...me, savedBooks:[ ...me.savedBooks, bookToSave] }}})
+      }
+    });
       // const response = await saveBook(bookToSave, token);
 
       // if (!response.ok) {
